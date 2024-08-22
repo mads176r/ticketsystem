f6968479513a0072479e2f47ec05a40c77d8da87
Ikhvan <ikhvan95@gmail>
2024-08-22 18:52:25 +0200

    Added smooth transition between pages

--- src/components/pagetransition.tsx
+++ src/components/pagetransition.tsx
@@ -8,10 +8,10 @@ interface PageTransitionProps {
 export default function PageTransition({ children }: PageTransitionProps) {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 2.5 }}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.5 }}
       className="min-h-screen"
     >
       {children}
